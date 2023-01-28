3df9bd0b77024d9e78113c1270046dcccd742581
victor2905 <98538867+victor2905@users.noreply.github.com>
2023-01-27 22:35:32 -0500

    entrada_datos2
    Creación de proyecto

---
+++ entrada_datos2.py
@@ -0,0 +1,9 @@
+nombre = input("¿Cuál es tu nombre?")
+print("Hola  "+ nombre +", vamos a realizar una suma.")
+
+num_uno = int(input("Ingrese el primer número: "))
+num_dos = int(input("Ingrese el segundo número: "))
+
+resultado = num_uno + num_dos
+
+print("El resultado es: ",resultado)
